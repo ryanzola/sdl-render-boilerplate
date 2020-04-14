0921776d4b9245327d1d0e1f3174c733cf161ae4
ryanzola <ryanzola@me.com>
2020-04-13 21:15:41 -0400

    Separation of concerns

--- src/main.c
+++ src/main.c
@@ -1,56 +1,19 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <SDL2/SDL.h>
+#include "display.h"
 
 bool is_running = false;
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
-
-uint32_t* color_buffer = NULL;
-int window_width = 800;
-int window_height = 600;
-
-bool initialize_window(void) {
-  if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-    fprintf(stderr, "Error initializing SDL.\n");
-    return false;
-  }
-
-  /* Create SDL Window */
-  window = SDL_CreateWindow(
-    NULL,
-    SDL_WINDOWPOS_CENTERED,
-    SDL_WINDOWPOS_CENTERED,
-    window_width,
-    window_height,
-    SDL_WINDOW_BORDERLESS
-  );
-
-  if(!window) {
-    fprintf(stderr, "Error creating SDL window\n");
-
-    return false;
-  }
-
-  /* Create SDL Renderer */
-  renderer = SDL_CreateRenderer(
-    window,
-    -1,
-    0
-  );
-
-  if(!renderer) {
-    fprintf(stderr, "Error creating SDL renderer\n");
-
-    return false;
-  }
-
-  return true;
-}
 
 void setup(void) {
+  // allocate the required memory in bytes to hold the color buffer
   color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
+
+  // create an SDL texture that is used to display the color buffer
+  color_buffer_texture = SDL_CreateTexture(
+    renderer,
+    SDL_PIXELFORMAT_ARGB8888,
+    SDL_TEXTUREACCESS_STREAMING,
+    window_width,
+    window_height
+  );
 }
 
 void process_input(void) {
@@ -70,21 +33,20 @@ void process_input(void) {
 }
 
 void update(void) {
-
-} 
-
-void render(void) {
-  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-  SDL_RenderClear(renderer);
-
-  SDL_RenderPresent(renderer);
+  // TODO:
 }
 
-void destroy_window(void) {
-  free(color_buffer);
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+void render(void) {
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
+
+  draw_grid();
+  draw_rect(300, 200, 300, 150, 0xFFFF00FF);
+
+  render_color_buffer();
+  clear_color_buffer(0xFF000000);
+
+  SDL_RenderPresent(renderer);
 }
 
 int main(void) {
